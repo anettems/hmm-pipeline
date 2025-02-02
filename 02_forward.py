790e9188c7d1b8288a746554e908adaa8c2d1df2
anettems <sarivuoanette@gmail.com>
2025-02-02 16:02:27 +0200

    Added source space creation for template subject

--- 02_forward.py
+++ 02_forward.py
@@ -25,6 +25,27 @@ elif spacing=='ico5':
     ico=5
 
 
+# Create the source space for the template subject
+sessions = ["01"]#, "03", "04", "05"]
+subject = 'fsaverage_sara' 
+
+for ses in sessions:
+    # Create folder if it does not exist
+    if not os.path.exists(fname.hmm_bids_dir(subject=subject, ses=ses) + '/forward/'):  
+        os.makedirs(fname.hmm_bids_dir(subject=subject,ses=ses) + '/forward/')
+    
+    megdir=fname.hmm_bids_dir(subject=subject, ses=ses)
+    
+    if os.path.exists(fname.src(hmm_bids_dir=megdir, subject=subject,spacing=spacing)):
+        subject_src = mne.read_source_spaces(fname.src(hmm_bids_dir=megdir, subject=subject,spacing=spacing))
+        bem = mne.read_bem_solution(fname.bem_sol(hmm_bids_dir=megdir, subject=subject,ntri=ntri))
+    else:
+        subject_src = mne.setup_source_space(subject=subject, spacing=spacing,
+                                    subjects_dir=subjects_dir_ave,
+                                    n_jobs=N_JOBS, add_dist=True)
+        mne.write_source_spaces(fname.src(hmm_bids_dir=megdir, subject=subject,spacing=spacing), subject_src, overwrite=True)
+        print("Source space for template subject created.")
+        
 # Read the peak channel csv
 df_subjects = pd.read_csv("subject_text_files/test.txt", names=["subject"])
 sessions = ["01"]#, "03", "04", "05"]
